6ee395006a94ae183acb8da55ddf581ec27e8226
EmanuelAntonio <emanu.jf@hotmail.com>
2017-08-30 23:25:28 -0300

    trie sem recursao na inserção

--- Trie.cpp
+++ Trie.cpp
@@ -1,7 +1,9 @@
 #include "Trie.h"
 
-Trie::Trie() { raiz = new No_Trie(); altura = 0; }
-
+Trie::Trie(){
+    raiz = new No_Trie();
+    altura = 0;
+}
 /** \brief
  *  Função para fazer a inserção de lexemas dentro da tabela de simbolos
  * \param
@@ -12,38 +14,33 @@ Trie::Trie() { raiz = new No_Trie(); altura = 0; }
  *  vazio
  */
 void Trie::insere(char* entrada, Atributo* atributo){
-    insereRecursivo(entrada, 0, raiz, atributo);
-}
 
-/** \brief
- *  Função auxiliar para o metodo de inserção
- * \param
- *  entrada: lexema a ser inserido na tabela
- *  indice: indice da entrada a ser analisada
- *  atributo: atributo associado ao lexema
- *  pai: nó da árvore trie a ser analisado
- * \param
- * \return
- *  Ponteiro para o nó que foi analisado
- *  vazio
- */
-void Trie::insereRecursivo(char* entrada, int indice, No_Trie *pai, Atributo* atributo){
-    /// se caso tenha chegado ao fim da string, termina a recursão e marca nó como chave
-    if(entrada[indice] == '\0'){
-        pai->atribuirChave(true);
-        pai->atribuirAtributo(atributo);
-        if(altura < indice + 1) { altura = indice + 1; }
-    }
-    else{
-        No_Trie *filho = pai->pegarCaractere(entrada[indice]);
-        if(filho == NULL){ ///Se o nó associado ao caractere da entrada nao existir, o cria
-            filho = new No_Trie();
-            pai->atribuirCaractere(entrada[indice], filho);
+    int indice = 0;
+    No_Trie *no = raiz;
+    No_Trie *auxiliar = NULL;
+    char caractere;
+
+    while(entrada[indice] != '\0'){
+
+        caractere = entrada[indice];
+        auxiliar = no->pegarCaractere(caractere);
+        if(auxiliar == NULL){
+            auxiliar = new No_Trie();
+            no->atribuirCaractere(caractere, auxiliar);
+            no = auxiliar;
+        }else{
+            no = no->pegarCaractere(caractere);
         }
-        insereRecursivo(entrada, indice + 1, filho,atributo);
+        indice++;
     }
-}
+    if(altura < indice + 1){
 
+        altura = indice + 1;
+
+    }
+    no->atribuirChave(true);
+    no->atribuirAtributo(atributo);
+}
 /** \brief
  *  Função para imprimir todos os lexemas guardados dentro da tabela
  * \param
@@ -56,7 +53,6 @@ void Trie::imprime(){
     char saida[altura + 1];
     imprimeRecursivo(saida, 0, raiz);
 }
-
 /** \brief
  *  Função recursiva auxiliar para percorrer a trie
  * \param
@@ -79,31 +75,6 @@ void Trie::imprimeRecursivo(char* saida, int indice, No_Trie *n){
         }
     }
 }
-
-/** \brief
- *  Função recursiva auxiliar que percorre a trie
- * \param
- *  entrada: lexema a ser buscado
- *  indice: indice da string de entrada que será analisado
- *  pai: no da trie a ser analisado
- * \param
- * \return
- *  Atributo associado ao lexema buscado
- */
-Atributo* Trie::buscaRecursivo(char *entrada, int indice, No_Trie *pai){
-    if(pai != NULL){                 /// se chagar em um nó nulo o lexema não existe na tabela
-        if(entrada[indice] == '\0'){ /// se chaegar no fim da string dentro de um nó existente
-            if(pai->EChave()){       /// e esse nó for uma chave, encontrei o lexema na tabela de simbolos
-                return pai->pegarAtributo();
-            }
-        }
-        else{                        ///se não, desce mais um nível na árvore
-            return buscaRecursivo(entrada, indice + 1, pai->pegarCaractere(entrada[indice]));
-        }
-    }
-    return NULL;
-}
-
 /** \brief
  *  Função que busca um lexema na trie e retorna o atributo associado a ele
  * \param
@@ -113,7 +84,19 @@ Atributo* Trie::buscaRecursivo(char *entrada, int indice, No_Trie *pai){
  *  Atributo associado a esse lexema
  */
 Atributo* Trie::busca(char *entrada){
-    return buscaRecursivo(entrada, 0, raiz);
+    int indice = 0;
+    No_Trie *no = raiz;
+    while(entrada[indice] != '\0'){
+        no = no->pegarCaractere(entrada[indice]);
+        if(no == NULL){
+            return NULL;
+        }
+        indice++;
+    }
+    if(no->EChave()){
+        return no->pegarAtributo();
+    }
+    return NULL;
 }
 int Trie::calcularTamanhoLexema(char *lexema){
     int total=0;
--- Trie.h
+++ Trie.h
@@ -1,7 +1,6 @@
 #ifndef TRIE_H
 #define TRIE_H
 #include "No_Trie.h"
-#include "Tokens.h"
 
 class Trie{
     public:
@@ -10,16 +9,15 @@ class Trie{
         void insere(char* entrada, Atributo* atributo);
         void imprime();
         virtual ~Trie();
+
     protected:
         int altura;
         No_Trie *raiz;
-        int calcularTamanhoLexema(char* lexema);
         virtual void imprimeLexema(char* entrada, Atributo * atr);
         virtual void imprimeCabecalho();
+        int calcularTamanhoLexema(char *str);
     private:
-        void insereRecursivo(char* entrada, int indice, No_Trie *raiz, Atributo* atributo);
         void imprimeRecursivo(char* saida, int indice, No_Trie *n);
-        Atributo *buscaRecursivo(char *entrada, int indice, No_Trie *pai);
 };
 
 #endif // TRIE_H
