300d4ad610f9e5edd0add6f94ec66498a327dbd5
EmanuelAntonio <emanu.jf@hotmail.com>
2017-10-09 20:53:12 -0300

    Construtores da ASA

--- Novo Projeto/ASA.cpp
+++ Novo Projeto/ASA.cpp
@@ -4,6 +4,155 @@ static TabelaInteiro tabelaInteiro;
 static TabelaLiteral tabelaLiteral;
 static TabelaReal tabelaReal;
 
+
+Programa::Programa(DeclClasse *listaClasse, DeclFuncao *listaFuncao, DeclTipo *listaTipo, DeclVariavel *listaVariavel){
+    this->listaClasse = listaClasse;
+    this->listaFuncao = listaFuncao;
+    this->listaTipo = listaTipo;
+    this->listaVariavel = listaVariavel;
+}
+Literal::Literal(char *literal){
+    this->literal = literal;
+}
+Ascii::Ascii(char caractere){
+    this->caractere = caractere;
+}
+Parenteses::Parenteses(Expr expressao){
+    this->expressao = expressao;
+}
+Conteudo::Conteudo(Primario primario){
+    this->primario = primario;
+}
+Endereco::Endereco(Primario primario){
+    this->primario = primario;
+}
+NumInteiro::NumInteiro(int num){
+    this->num = num;
+}
+NumReal::NumReal(float num){
+    this->num = num;
+}
+Arranjo::Arranjo(NumInteiro num):num(num){
+    this->num = num;
+}
+ListaExpr::ListaExpr(Expr expressao, ListaExpr *lista){
+    this->expressao = expressao;
+    this->lista = lista;
+}
+ListaFormal::ListaFormal(int tipo, bool ponteiro, Id id, Arranjo *arranjo, ListaFormal *lista){
+    this->tipo = tipo;
+    this->ponteiro = ponteiro;
+    this->id = id;
+    this->arranjo = arranjo;
+    this->lista = lista;
+}
+ListaSentenca::ListaSentenca(Sentenca sentenca, ListaSentenca *lista){
+    this->sentenca = sentenca;
+    this->lista = lista;
+}
+If::If(Expr expressao, Sentenca sentenca){
+    this->expressao = expressao;
+    this->sentenca = sentenca;
+}
+Else::Else(Sentenca sentenca){
+    this->sentenca = sentenca;
+}
+While::While(Expr expressao, Sentenca sentenca){
+    this->expressao = expressao;
+    this->sentenca = sentenca;
+}
+BlocoCaso::BlocoCaso(Num num, ListaSentenca *listaSentenca, BlocoCaso *lista){
+    this->num = num;
+    this->listaSentenca = listaSentenca;
+    this->lista = lista;
+}
+Switch::Switch(Expr expressao, BlocoCaso blocoCaso):blocoCaso(blocoCaso){
+    this->expressao = expressao;
+    this->blocoCaso = blocoCaso;
+}
+Print::Print(ListaExpr *listaExpr){
+    this->listaExpr = listaExpr;
+}
+ReadLn::ReadLn(Expr expressao){
+    this->expressao = expressao;
+}
+Return::Return(Expr expressao){
+    this->expressao = expressao;
+}
+Escopo::Escopo(ListaSentenca *lista){
+    this->lista = lista;
+}
+ChamadaFuncao::ChamadaFuncao(Id id, ListaExpr *parametros){
+    this->id = id;
+    this->parametros = parametros;
+}
+Try::Try(Sentenca sentencaTry, Sentenca sentencaCatch){
+    this->sentencaTry = sentencaTry;
+    this->sentencaCatch = sentencaCatch;
+}
+SentencaExpr::SentencaExpr(Expr expressao){
+    this->expressao = expressao;
+}
+DeclFuncao::DeclFuncao(int tipo, Id id, ListaFormal *parametros, DeclVariavel *variaveis, ListaSentenca sentenca, DeclFuncao *lista):sentenca(sentenca){
+    this->tipo = tipo;
+    this->id = id;
+    this->parametros = parametros;
+    this->variaveis = variaveis;
+    this->sentenca = sentenca;
+    this->lista = lista;
+}
+ListaId::ListaId(ListaId *lista, bool ponteiro, Arranjo *arranjo){
+    this->lista = lista;
+    this->ponteiro = ponteiro;
+    this->arranjo = arranjo;
+}
+DeclVariavel::DeclVariavel(int tipo, ListaId variaveis, DeclVariavel *lista):variaveis(variaveis){
+    this->tipo = tipo;
+    this->variaveis = variaveis;
+    this->lista = lista;
+}
+DeclTipo::DeclTipo(DeclVariavel *campo){
+    this->campo = campo;
+}
+DeclLocalFuncao::DeclLocalFuncao(DeclFuncao funcao, DeclLocal *lista):funcao(funcao){
+    this->funcao = funcao;
+    this->lista = lista;
+}
+DeclLocalVariavel::DeclLocalVariavel(DeclVariavel variavel, DeclLocal *lista):variavel(variavel){
+    this->variavel = variavel;
+    this->lista = lista;
+}
+DeclLocalPublic::DeclLocalPublic(DeclLocal *lista){
+    this->lista = lista;
+}
+DeclLocalPrivate::DeclLocalPrivate(DeclLocal *lista){
+    this->lista = lista;
+}
+DeclClasse::DeclClasse(Id id, Id *heranca, DeclLocal *lista){
+    this->id = id;
+    this->heranca = heranca;
+    this->lista = lista;
+}
+ExprUnaria::ExprUnaria(int operador, Expr expressao){
+    this->operador = operador;
+    this->expressao = expressao;
+}
+ExprBinaria::ExprBinaria(int operador, Expr exprEsquerda, Expr exprDireita){
+    this->operador = operador;
+    this->exprEsquerda = exprEsquerda;
+    this->exprDireita = exprDireita;
+}
+ExprAtrib::ExprAtrib(Expr exprEsquerda, Expr exprDireita){
+    this->exprEsquerda = exprEsquerda;
+    this->exprDireita = exprDireita;
+}
+ExprAceCamp::ExprAceCamp(Expr exprEsquerda, Expr exprDireita){
+    this->exprEsquerda = exprEsquerda;
+    this->exprDireita = exprDireita;
+}
+New::New(ListaExpr *listaExpr){
+    this->listaExpr = listaExpr;
+}
 TabelaIdentificador* obtemTabelaIdentificador(){return &tabelaID;};
 TabelaLiteral* obtemTabelaLiteral(){return &tabelaLiteral;};
 TabelaInteiro* obtemTabelaInteiro(){return &tabelaInteiro;};
--- Novo Projeto/ASA.h
+++ Novo Projeto/ASA.h
@@ -9,6 +9,7 @@ class DeclClasse;
 
 class Programa{
     public:
+        Programa(DeclClasse *listaClasse, DeclFuncao *listaFuncao, DeclTipo *listaTipo, DeclVariavel *listaVariavel);
         DeclClasse *listaClasse;
         DeclFuncao *listaFuncao;
         DeclTipo *listaTipo;
@@ -19,43 +20,54 @@ class Expr{};
 class Primario:public Expr{};
 class Literal:public Primario{
     public:
+        Literal(char *literal);
         char* literal;
 };
 class Ascii:public Primario{
     public:
+        Ascii(char caractere);
         char caractere;
 };
 class Parenteses:public Primario{
     public:
+        Parenteses(Expr expressao);
         Expr expressao;
 };
 class Conteudo:public Primario{
     public:
+        Conteudo(Primario primario);
         Primario primario;
 };
-class Entereco:public Primario{
+class Endereco:public Primario{
     public:
+        Endereco(Primario primario);
         Primario primario;
 };
 class Num:public Primario{};
 class NumInteiro:public Num{
     public:
+        NumInteiro(int num);
         int num;
 };
 class NumReal:public Num{
     public:
+        NumReal(float num);
         float num;
 };
 class Arranjo{
-    NumInteiro num;
+    public:
+        Arranjo(NumInteiro num);
+        NumInteiro num;
 };
 class ListaExpr{
     public:
+        ListaExpr(Expr expressao, ListaExpr *lista);
         Expr expressao;
         ListaExpr *lista;
 };
 class ListaFormal{
     public:
+        ListaFormal(int tipo, bool ponteiro, Id id, Arranjo *arranjo, ListaFormal *lista);
         int tipo;
         bool ponteiro;
         Id id;
@@ -65,134 +77,160 @@ class ListaFormal{
 class Sentenca{};
 class ListaSentenca{
     public:
+        ListaSentenca(Sentenca sentenca, ListaSentenca *lista);
         Sentenca sentenca;
         ListaSentenca *lista;
 };
 class If:public Sentenca{
     public:
+        If(Expr expressao, Sentenca sentenca);
         Expr expressao;
         Sentenca sentenca;
 };
 class Else:public Sentenca{
     public:
+        Else(Sentenca sentenca);
         Sentenca sentenca;
 };
 class While:public Sentenca{
     public:
+        While(Expr expressao, Sentenca sentenca);
         Expr expressao;
         Sentenca sentenca;
 };
 class BlocoCaso{
     public:
+        BlocoCaso(Num num, ListaSentenca *listaSentenca, BlocoCaso *lista);
         Num num;
         ListaSentenca *listaSentenca;
         BlocoCaso *lista;
 };
 class Switch:public Sentenca{
     public:
+        Switch(Expr expressao, BlocoCaso blocoCaso);
         Expr expressao;
         BlocoCaso blocoCaso;
 };
 class Break:public Sentenca{};
 class Print:public Sentenca{
     public:
+        Print(ListaExpr *listaExpr);
         ListaExpr *listaExpr;
 };
 class ReadLn:public Sentenca{
     public:
+        ReadLn(Expr expressao);
         Expr expressao;
 };
 class Return:public Sentenca{
     public:
+        Return(Expr expressao);
         Expr expressao;
 };
 class Throw:public Sentenca{};
 class Escopo:public Sentenca{
     public:
+        Escopo(ListaSentenca *lista);
         ListaSentenca *lista;
 };
 class ChamadaFuncao:public Sentenca{
     public:
+        ChamadaFuncao(Id id, ListaExpr *parametros);
         Id id;
         ListaExpr *parametros;
 };
 class Try:public Sentenca{
     public:
+        Try(Sentenca sentencaTry, Sentenca sentencaCatch);
         Sentenca sentencaTry;
         Sentenca sentencaCatch;
 };
 class SentencaExpr:public Sentenca{
     public:
+        SentencaExpr(Expr expressao);
         Expr expressao;
 };
 class DeclFuncao{
     public:
+        DeclFuncao(int tipo, Id id, ListaFormal *parametros, DeclVariavel *variaveis, ListaSentenca sentenca, DeclFuncao *lista);
         int tipo;
         Id id;
         ListaFormal *parametros;
         DeclVariavel *variaveis;
         ListaSentenca sentenca;
-        DeclFuncao *funcao;
+        DeclFuncao *lista;
 };
 class ListaId{
     public:
-        ListaId *listaId;
+        ListaId(ListaId *lista, bool ponteiro, Arranjo *arranjo);
+        ListaId *lista;
         bool ponteiro;
         Arranjo *arranjo;
 };
 class DeclVariavel{
     public:
+        DeclVariavel(int tipo, ListaId variaveis, DeclVariavel *lista);
         int tipo;
         ListaId variaveis;
-        DeclVariavel *listaVariaveis;
+        DeclVariavel *lista;
 };
 class DeclTipo{
     public:
+        DeclTipo(DeclVariavel *campo);
         DeclVariavel *campo;
 };
 class DeclLocal{};
 class DeclLocalFuncao:public DeclLocal{
     public:
+        DeclLocalFuncao(DeclFuncao funcao, DeclLocal *lista);
         DeclFuncao funcao;
         DeclLocal *lista;
 };
 class DeclLocalVariavel:public DeclLocal{
     public:
+        DeclLocalVariavel(DeclVariavel variavel, DeclLocal *lista);
         DeclVariavel variavel;
         DeclLocal *lista;
 };
 class DeclLocalPublic:public DeclLocal{
     public:
+        DeclLocalPublic(DeclLocal *lista);
         DeclLocal *lista;
 };
 class DeclLocalPrivate:public DeclLocal{
     public:
+        DeclLocalPrivate(DeclLocal *lista);
         DeclLocal *lista;
 };
 class DeclClasse{
     public:
+        DeclClasse(Id id, Id *heranca, DeclLocal *lista);
         Id id;
         Id *heranca;
-        DeclLocal lista;
+        DeclLocal *lista;
 };
 class ExprUnaria:public Expr{
     public:
+        ExprUnaria(int operador, Expr expressao);
         int operador;
         Expr expressao;
 };
 class ExprBinaria:public Expr{
     public:
+        ExprBinaria(int operador, Expr exprEsquerda, Expr exprDireita);
         int operador;
         Expr exprEsquerda;
         Expr exprDireita;
 };
 class ExprAtrib:public Expr{
     public:
+        ExprAtrib(Expr exprEsquerda, Expr exprDireita);
         Expr exprEsquerda;
         Expr exprDireita;
 };
 class ExprAceCamp:public Expr{
     public:
+        ExprAceCamp(Expr exprEsquerda, Expr exprDireita);
         Expr exprEsquerda;
         Expr exprDireita;
 };
@@ -201,6 +239,7 @@ class False:public Primario{};
 class This:public Primario{};
 class New:public Primario{
     public:
+        New(ListaExpr *listaExpr);
         ListaExpr *listaExpr;
 };
 
