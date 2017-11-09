1979ed5b9023126860f87d952279f0805d744a63
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2017-11-09 19:02:02 -0200

    Fiz algo

--- Novo Projeto/VisitanteRI.cpp
+++ Novo Projeto/VisitanteRI.cpp
@@ -1,1 +1,90 @@
 #include "VisitanteRI.h"
+#include "RepresentacaoIntermadiaria.h"
+ESEQ::ESEQ(Stm *sentenca, Exp *exp){
+    this->s=sentenca;
+    this->e=exp;
+};
+ESEQ::~ESEQ(){
+    delete s;
+    delete e;
+};
+void ESEQ::aceita(VisitanteRI *vri){
+    vri->visita(this);
+};
+ListaStm::ListaStm(Stm *stm, ListaStm *proximoStm){
+    this->stm=stm;
+    this->proximoStm=proximoStm;
+};
+ListaStm::~ListaStm(){
+    delete stm;
+    delete proximoStm;
+};
+void ListaStm::aceita(VisitanteRI *vri){
+    //vri->visita(this);
+};
+MOVE::MOVE(Exp *e1, Exp *e2){
+    this->e1=e1;
+    this->e2=e2;
+};
+MOVE::~MOVE(){
+    delete e1;
+    delete e2;
+};
+void MOVE::aceita(VisitanteRI *vri){
+    vri->visita(this);
+};
+
+EXP::EXP(Exp *e){
+    this->e=e;
+};
+EXP::~EXP(){
+    delete e;
+};
+void EXP::aceita(VisitanteRI *vri){
+    vri->visita(this);
+};
+JUMP::JUMP(Exp *e){
+    this->e=e;
+};
+JUMP::~JUMP(){
+    delete e;
+};
+void JUMP::aceita(VisitanteRI *vri){
+    vri->visita(this);
+};
+CJUMP::CJUMP(int op, Exp *e1, Exp *e2, Rotulo *verdadeiro, Rotulo *falso){
+    this->op=op;
+    this->e1=e1;
+    this->e2=e2;
+    this->verdadeiro=verdadeiro;
+    this->falso=falso;
+};
+CJUMP::~CJUMP(){
+    delete e1;
+    delete e2;
+    delete verdadeiro;
+    delete falso;
+};
+void CJUMP::aceita(VisitanteRI *vri){
+    vri->visita(this);
+};
+SEQ::SEQ(Stm *s1, Stm *s2){
+    this->s1=s1;
+    this->s2=s2;
+};
+SEQ::~SEQ(){
+    delete s1;
+    delete s2;
+};
+void SEQ::aceita(VisitanteRI *vri){
+
+};
+LABEL::LABEL(Rotulo *n){
+    this->n=n;
+};
+LABEL::~LABEL(){
+    delete n;
+};
+void LABEL::aceita(VisitanteRI *vri){
+    vri->visita(this);
+};
